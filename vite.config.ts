0b5719fc47f04bc79f2fab7c04880b9442de5286
DanielMEH <jdospina943@misena.edu.co>
2024-02-18 10:44:06 -0500

    versionando scripts sql v1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
+
 export default defineConfig({
+  esbuild: {
+    target: 'esnext',
+  },
   plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+    
+  },
 })
