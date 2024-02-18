ad7be8072e4883b7d58303ceecb3cbbdb18f80b5
DanielMEH <jdospina943@misena.edu.co>
2024-02-18 11:09:39 -0500

    upload proyect ENV VITE

--- src/services/ServiceUsers.tsx
+++ src/services/ServiceUsers.tsx
@@ -3,16 +3,19 @@ import { ApiDelete, ApiPost, ApiPut } from "../api/Api";
 import datajson from "../data/data.json";
 
 
-if (process.env.URL_CATALOGS){
-    datajson.url_servers.url_catalogs = `${process.env.URL_CATALOGS}`
+if (import.meta.env.VITE_URL_CATALOGS){
+    datajson.url_servers.url_catalogs = import.meta.env.VITE_URL_CATALOGS;
 }
 
- class ServiceUsers {
+
+class ServiceUsers {
     public async getLoginUser(email: string, password: string): Promise<AxiosResponse> {
         const resp = await ApiPost(datajson.url_servers.url_catalogs, "login", "POST", "", {email, password});
         return resp;
     }
     public async postRegisterUser(email:string, password:string): Promise<AxiosResponse>  {
+        console.log(import.meta.env)
+        console.log("URL_CATALOGS", datajson.url_servers.url_catalogs)
         return await ApiPost(datajson.url_servers.url_catalogs, "register", "POST", "", {email, password});
     }
     public async getProfileUser(): Promise<AxiosResponse> {
